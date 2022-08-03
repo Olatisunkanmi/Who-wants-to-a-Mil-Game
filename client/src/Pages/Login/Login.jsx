d5b1607c0c06bd7cceb8c052823b6a2e62467a23
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-03 18:03:33 +0100

    Update Login.jsx

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
               try {      
                 // To auth user if !user 
                 // send a login error 
-                  const res = await axios.post('/auth/login',  {
+                  const res = await axios.get('/auth/login',  {
                    username : userRef.current.value,
                    password : passwordRef.current.value,
                  }) 
