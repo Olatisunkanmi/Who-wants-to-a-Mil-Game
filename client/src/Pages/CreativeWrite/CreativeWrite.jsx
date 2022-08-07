ac16121304fa46482b5b7365f0c58d1a51e8e633
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-07 22:08:05 +0100

    Update CreativeWrite.jsx

--- client/src/Pages/CreativeWrite/CreativeWrite.jsx
+++ client/src/Pages/CreativeWrite/CreativeWrite.jsx
@@ -4,78 +4,15 @@ import axios from "axios";
 import { useState } from "react"
 
 
-// If page is yet to return render loading
-
 export default function CreativeWrite () {
-  const [posts, setPosts ] = useState([]);
-const [newPost, setNewPosts ] =  useState([])
 
-
-
-  useEffect(() => {
-      const fetchPosts =  async () => {
-              
-                try {
-              const res = await axios.get("/posts" );
-                  console.log(res);
-                                      try {
- 
-                                        let   newArr =  res.data;
-                                        // A function to randomly select three posts for the cardlists
-                                        
-                                        const chooseRandom = (arr, num = 1) => {
-                                          const res = [];
-                                          for(let i = 0; i < num; ){
-                                            const random = Math.floor(Math.random() * arr.length);
-                                            if(res.indexOf(arr[random]) !== -1){
-                                                continue;
-                                            };
-                                            res.push(arr[random]);
-                                            i++;
-                                          };
-                                          return res;
-                                      };
-                                    
-                                      setPosts(chooseRandom(newArr, 3));
-                                      } catch (error) {
-                                             
-                                      }
-
-
-
-
-
-
-              try {
-                                // getting the last post to send to <FirstCard />
-                                 // finding the last post of the array
-              const newPost =  res.data[res.data.length - 1];
-                              // console.log(newPost);
-                              setNewPosts(newPost)
-                          } catch (error) {
-                              console.log(error);
-                          }
-            // setting post to the newdata received from the /api
-           
-        
-           } catch (error) {
-              console.log(error);
-           }
-      }
-
-      fetchPosts()
-  }, [])
-
-
- 
 
   return (
         <div>
 
     <Header />
     <Topbar  /> 
-    <FirstCard  key={newPost.title}  newPost={newPost}/>
-    <FeaturedCard  key={posts.title }   posts={posts }/>
+    <FirstCard />
     <Footer /> 
 
         </div>
