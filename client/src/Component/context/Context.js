70b515b616c1cb0d453ac78857c86b776e71db49
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-07 09:44:50 +0100

    Update Context.js

--- client/src/Component/context/Context.js
+++ client/src/Component/context/Context.js
@@ -1,5 +1,6 @@
-import { createContext, useEffect, useReducer } from "react";  
+import { createContext, useEffect, useReducer, useState } from "react";  
 import Reducer from "./Reducer";
+import axios from 'axios'
 
 // user :null here because no user until login.
 const INITIAL_STATE = {
@@ -17,13 +18,28 @@ export const Context = createContext(INITIAL_STATE);
 
 export const ContextProvider = ({ children }) => {    
         const [state, dispatch ] = useReducer(Reducer, INITIAL_STATE);
+        const [post , setPost ] = useState([])
 
+
+        useEffect(() => {
+        const getDetails = async () => {
+
+        const res = await axios.get('https://cracked-ink-cv.herokuapp.com/api/posts');    
+        // setPost(res.data);
+        console.log(res.data)
+
+        };
+        getDetails();  
+        }, [post])
+  
 
         // this UseEffect fn is started when there is change to our user
     useEffect(() => {
         localStorage.setItem("user",  JSON.stringify( state.user ))
        
     })
+
+    
 
         return (
             <Context.Provider   
