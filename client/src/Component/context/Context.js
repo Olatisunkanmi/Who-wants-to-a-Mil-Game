37d1f769f83775fbf7971f78d4859f0e18023906
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-08 10:36:11 +0100

    Update Context.js

--- client/src/Component/context/Context.js
+++ client/src/Component/context/Context.js
@@ -57,8 +57,6 @@ export const ContextProvider = ({ children }) => {
                         newPost,
                         setNewPost,
                         RandomPosts,
-                        
-                    
             }} 
          >  
             {children}
