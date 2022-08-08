5effd7b536d35eab66913227884afb396579103b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-08 11:12:20 +0100

    Update Topbar.jsx

--- client/src/Component/Topbar.jsx
+++ client/src/Component/Topbar.jsx
@@ -7,47 +7,57 @@ const Topbar = () => {
         <div 
                 className=''>
                         <div 
-                                className='max-w-5xl mx-auto justify-around flex flex-row p-5 mt-7 border-4 border-y-black
+                                className='
+                                lg:w-4/6
+                                m-auto 
+                                md:justify-around 
+                                flex flex-row 
+                                p-5 
+                                mt-7 border-4 border-y-black
                                 border-x-0'>
                                 <ul 
-                                        className='flex flex-row w-3/5 text-center justify-between '>
+                                        className='flex flex-row 
+                                                        w-full
+                                                        md:w-5/6 text-md  
+                                                        text-center justify-between  '>
                                         <li  > 
-                                                 <a href='/home' className='font-serif cursor-pointer text-xl
+                                                 <a href='/home' className='font-serif cursor-pointer md:text-xl
                                                 font-semibold
+                                                
                                                 hover:text-veryLightBrown' > Home </a>
                                         </li>
 
                                         <li> 
-                                                        <a href='/posts' className='font-serif cursor-pointer text-xl
+                                                        <a href='/posts' className='font-serif cursor-pointer md:text-xl
                                                 font-semibold
                                                 hover:text-veryLightBrown' > Write ups</a>
                                         </li>
                                         <li> 
                                                
-                                                        <a href='' className='font-serif cursor-pointer text-xl
+                                                        <a href='' className='font-serif cursor-pointer md:text-xl
                                                 font-semibold
                                                 hover:text-veryLightBrown' > Services </a>
                                         </li>
                                         <li> 
                                              
-                                                        <a href='/'  className='font-serif cursor-pointer text-xl
+                                                        <a href='/'  className='font-serif cursor-pointer md:text-xl
                                                 font-semibold
                                                 hover:text-veryLightBrown'> About Me </a>
                                         </li>
                                       
+                                        <li >
+                                        <a
+                                        href='https://www.buymeacoffee.com/olatisun'
+                                        target='_blank'
+                                          className='font-serif cursor-pointer md:text-xl
+                                          font-bold
+                                          text-brightRed'> Buy ME Coffee <LocalCafeOutlined /> </a>
+                                        </li>
                                        
                                 </ul>
 
 
-                                <div    >
-                                      <a
-                                      href='https://www.buymeacoffee.com/olatisun'
-                                      target='_blank'
-                                        className='font-serif cursor-pointer text-xl
-                                        font-bold
-                                        text-brightRed'> Buy ME Coffee <LocalCafeOutlined /> </a>
-                                        
-                                </div>
+                             
                         </div>
         </div>
         
