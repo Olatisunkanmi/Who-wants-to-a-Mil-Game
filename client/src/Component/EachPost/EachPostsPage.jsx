2e6afe16002683e9712ef86d401c57350fe560a3
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-08 10:36:14 +0100

    Update EachPostsPage.jsx

--- client/src/Component/EachPost/EachPostsPage.jsx
+++ client/src/Component/EachPost/EachPostsPage.jsx
@@ -9,15 +9,19 @@ import {Context } from '../context/Context'
 
 const Posts = () => {
   const { Allpost}  = useContext(Context)
+
   return (
     <div  >
         <Header />
         <Topbar  /> 
 
+          <div className="w-1/2 flex flex-row m-auto " >
           {Allpost.map((cur ) => ( 
             <AllPostsCard key={cur._id}
             post={cur} />
           ))}
+
+         </div>
         
       
         <Footer /> 
