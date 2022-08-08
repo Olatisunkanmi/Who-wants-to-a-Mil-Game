dad82339e5edda8180f085777a9dacfd27abef41
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-08 10:48:45 +0100

    Update EachPostsPage.jsx

--- client/src/Component/EachPost/EachPostsPage.jsx
+++ client/src/Component/EachPost/EachPostsPage.jsx
@@ -15,7 +15,7 @@ const Posts = () => {
         <Header />
         <Topbar  /> 
 
-          <div className="w-1/2 flex flex-row m-auto " >
+          <div className="w-11/12 flex flex-col items-center lg:flex-row m-auto md:flex-wrap lg:w-4/6" >
           {Allpost.map((cur ) => ( 
             <AllPostsCard key={cur._id}
             post={cur} />
