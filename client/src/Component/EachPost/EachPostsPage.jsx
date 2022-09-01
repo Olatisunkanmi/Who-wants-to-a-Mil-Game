2b5f1cc2f2f2f84a9ab5448582417197f08bd900
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 09:38:57 +0100

    Update EachPostsPage.jsx

--- client/src/Component/EachPost/EachPostsPage.jsx
+++ client/src/Component/EachPost/EachPostsPage.jsx
@@ -1,31 +1,24 @@
-import { Topbar, Header, Footer,  AllPostsCard} from "../Index";
-import {useContext } from "react";
-import {Context } from '../context/Context'
-
-
-
-
+import { Topbar, Header, Footer, AllPostsCard } from '../Index';
+import { useContext } from 'react';
+import { Context } from '../context/Context';
 
 const Posts = () => {
-  const { Allpost}  = useContext(Context)
+	const { Allpost } = useContext(Context);
 
-  return (
-    <div  >
-        <Header />
-        <Topbar  /> 
+	return (
+		<div>
+			<Header />
+			<Topbar />
 
-          <div className="w-11/12 flex flex-col items-center lg:flex-row m-auto md:flex-wrap lg:w-4/6" >
-          {Allpost.map((cur ) => ( 
-            <AllPostsCard key={cur._id}
-            post={cur} />
-          ))}
+			<div className='w-11/12 flex flex-col items-center lg:flex-row m-auto md:flex-wrap lg:w-4/6'>
+				{Allpost.map((cur) => (
+					<AllPostsCard key={cur._id} post={cur} />
+				))}
+			</div>
 
-         </div>
-        
-      
-        <Footer /> 
-    </div>
-  )
-}
+			<Footer />
+		</div>
+	);
+};
 
-export default Posts
+export default Posts;
