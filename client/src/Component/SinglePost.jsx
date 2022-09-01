c74b7113ba308077b6a6aa86f0d2bd510604bd0c
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 09:39:14 +0100

    Update SinglePost.jsx

--- client/src/Component/SinglePost.jsx
+++ client/src/Component/SinglePost.jsx
@@ -1,214 +1,204 @@
- import { useAuth } from '../Hooks/useAuth';
-import   {EditOutlined } from '@material-ui/icons';
+import { useAuth } from '../Hooks/useAuth';
+import '../App.css';
+import { EditOutlined } from '@material-ui/icons';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 // import { Comment, MenuBook, Person, Timer } from '@material-ui/icons';
 
 const SinglePost = () => {
-  const [ SinglePost, setSinglePost ] = useState('')
-const location = useLocation().pathname.split('/')[2]
+	const [SinglePost, setSinglePost] = useState('');
+	const location = useLocation().pathname.split('/')[2];
 
-  // getting the Id of the post
-//    so all post and open on a new page in repect to their ids 
-useEffect(() => {
-  const getSinglePost = async () => {
-  const res = await axios.get(`https://cracked-ink-cv.herokuapp.com/api/posts/${location}`);    
-  setSinglePost(res.data)
+	// getting the Id of the post
+	//    so all post and open on a new page in repect to their ids
+	useEffect(() => {
+		const getSinglePost = async () => {
+			const res = await axios.get(
+				`https://cracked-ink-cv.herokuapp.com/api/posts/${location}`,
+			);
+			setSinglePost(res.data);
+		};
+		getSinglePost();
+	});
 
-  };
-  getSinglePost();  
-  }, )
+	const { user } = useAuth();
+	const PF = 'https://cracked-ink-cv.herokuapp.com/images/';
+	const [cat, setCats] = useState([]);
+	const [updatemode, setUpdateMode] = useState(false);
+	const [title, setTitle] = useState('');
+	const [desc, setDesc] = useState('');
+	const [comments, setComments] = useState([]);
 
+	// console.log(comments)
+	let COMMENTERROR = false;
+	const getCats = async () => {
+		// const res = await axios .get(`/posts`);
+	};
+	getCats();
 
+	const handleUpdate = async () => {
+		try {
+			const res = await axios.put(`/posts/${SinglePost._id}`, {
+				username: user.username,
+				title,
+				desc,
+			});
+			setUpdateMode(false);
+		} catch (error) {}
+	};
 
-  const {user } = useAuth()  
-  const PF =  "https://cracked-ink-cv.herokuapp.com/images/";
-  const [cat, setCats] =  useState([])
-  const [updatemode, setUpdateMode ] =  useState(false)
-  const [title, setTitle ] =  useState('')
-  const [desc, setDesc ] =  useState('')
-  const [comments, setComments] = useState([])
+	const postComment = async () => {
+		const username = document.querySelector('#username');
+		const comment = document.querySelector('#comment');
 
-  // console.log(comments)
- let COMMENTERROR = false
-      const getCats = async () => {
-      // const res = await axios .get(`/posts`);
-            
-      }
-      getCats()
+		if (username.value === '' || comment.value === '')
+			COMMENTERROR = true;
 
+		try {
+			const res = await axios.put(`/comment/${location}`, {
+				username: username.value,
+				comment: comment.value,
+			});
+			setComments(res.data.comments);
 
-  const handleUpdate = async() => {
-     try {
-      const res = await axios.put(`/posts/${SinglePost._id}`, {
-        username: user.username,
-        title,
-        desc
-      })  
-      setUpdateMode(false)
+			try {
+				username.value = ' ';
+				comment.value = '';
+			} catch (error) {}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<div>
+			<div className='text-center flex flex-col  items-center p-6 m-10'>
+				<img
+					src={PF + SinglePost.photo}
+					alt=''
+					className='w-full md:w-2/4 
+                                        text-center justify-center align-center object-cover'
+				/>
+				'
+				{updatemode ? (
+					<input
+						type='text'
+						value={SinglePost.title}
+						className=''
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				) : (
+					<p className='Phil mt-1 p-3 text-3xl md:text-5xl font-bold text-brightRed '>
+						{' '}
+						{SinglePost.title}{' '}
+					</p>
+				)}
+				<div className='mt-1 flex flex-row space-x-3 items-center font-bold text-veryLightBrown  sp'>
+					<p className='capitalize text-black text-lg'>
+						{' '}
+						{SinglePost.username}
+					</p>
+					<p className=''>
+						{new Date(SinglePost.createdAt).toDateString()}
+					</p>
 
-     } catch (error) {
-     
-     }
-  }
+					{user && (
+						<button
+							className='widgetSmButton'
+							onClick={() => setUpdateMode(true)}
+						>
+							<EditOutlined />
+						</button>
+					)}
+				</div>
+				<hr className='w-1/2  mt-10 border-2 border-veryLightBrown' />
+			</div>
 
-  const postComment = async () => {
-    const username =  document.querySelector('#username');
-    const comment =  document.querySelector('#comment');
+			<div className='text-left flex justify-center p-2 m-2 -mt-7'>
+				<div className='w-full md:w-2/3 leading-9 text-1xl md:text-xl font-serif'>
+					{updatemode ? (
+						<textarea
+							className='w-full h-full'
+							value={SinglePost.desc}
+							onChange={(e) => setDesc(e.target.value)}
+						/>
+					) : (
+						<p className='first-letter:ml-6 first-letter:text-4xl leading-loose'>
+							{' '}
+							{SinglePost.desc}{' '}
+						</p>
+					)}
 
-    if(username.value === "" || comment.value === "")
-    COMMENTERROR = true;
-       
-      try {
-        const res = await axios.put(`/comment/${location}`, {
-          username: username.value,
-        comment: comment.value
-        })
-        setComments(res.data.comments)
+					{updatemode && (
+						<button
+							className='p-3 font-bold  bg-black text-brightRed  
+                                                hover:bg-veryLightBrown hover:text-black'
+							onClick={handleUpdate}
+						>
+							Update
+						</button>
+					)}
+				</div>
+			</div>
 
-          try {
-           username.value = " "
-            comment.value = '';
-          } catch (error) {
-            
-          }
-        
-      } catch (error) {
-          console.log(error);
-      }
+			<hr className='w-1/2  my-10 border-2 border-veryLightBrown m-auto' />
+			{/**Comment section */}
 
-      
-  }
-  return (
-  
+			<div className='flex flex-col md:flex-row text-left w-3/5 m-auto mb-5 max-h-96'>
+				<div className='bg-stone-500 p-5 flex-wrap w-full md:w-2/6 space-y-2'>
+					<h3 className='post--comment--header text-white font-bold'>
+						Leave a Reply
+					</h3>
 
-    <div>
-                            <div className='text-center flex flex-col  items-center p-6 m-10'>
-                                        <img src={PF + SinglePost.photo} alt="" className='w-full md:w-2/4 
-                                        text-center justify-center align-center object-cover'/>
-                        '
-                                        {updatemode 
-
-                                          ? <input type='text' value={SinglePost.title} className='' onChange={(e) => setTitle(e.target.value)}  />
-                                          : <p className='mt-1 p-3 text-3xl md:text-5xl font-bold text-brightRed'> {SinglePost.title} </p>
-
-                                        }
-
-
-                                                    <div className='mt-1 flex flex-row space-x-3 items-center font-bold text-veryLightBrown  sp'>
-                                                            <p className='capitalize text-black text-lg' > {SinglePost.username}</p>
-                                                            <p className=''>
-                                                            {new Date(SinglePost.createdAt).toDateString() }</p> 
-                                                                
-                                                                
-                                                            {user && 
-                                                              <button className="widgetSmButton" onClick={() => setUpdateMode(true)}> 
-                                                              <EditOutlined />
-                                                              </button> }
-                                                </div>
-
-    <hr 
-                className='w-1/2  mt-10 border-2 border-veryLightBrown'/>
-                            </div>
-
-          
-
-                        <div className="text-left flex justify-center p-2 m-2 -mt-7">       
-                                      
-                                       <div className='w-full md:w-2/3 leading-9 text-1xl md:text-xl font-serif'>
-                                              {updatemode
-                                                ? <textarea className='w-full h-full' value={SinglePost.desc} onChange={(e) => setDesc(e.target.value)}  />
-
-                                                :<p className='first-letter:ml-6 first-letter:text-4xl leading-loose'>  {SinglePost.desc }  </p>
-
-                                              }
-
-                                              {updatemode &&
-
-                                                <button 
-                                                className='p-3 font-bold  bg-black text-brightRed  
-                                                hover:bg-veryLightBrown hover:text-black' 
-
-                                                onClick={handleUpdate}>
-                                                      Update
-                                                </button>
-                                                
-                                              }
-                                       </div>
-
-
-                                      
-                        </div>
-
-            
-              <hr 
-                   className='w-1/2  my-10 border-2 border-veryLightBrown m-auto'/>  
-                                              {/**Comment section */}
-
-                 <div 
-                   className='flex flex-col md:flex-row text-left w-3/5 m-auto mb-5 max-h-96' >
-                                              
-              
-
-                                    <div 
-                                      className='bg-stone-500 p-5 flex-wrap w-full md:w-2/6 space-y-2'>
-                                              <h3 className='post--comment--header text-white font-bold'>Leave a Reply</h3>
-                                              
-
-                                                <form className='flex flex-col space-y-10 '>
-                                                                    <input type="text" id="username" required placeholder='Your Name' className='
+					<form className='flex flex-col space-y-10 '>
+						<input
+							type='text'
+							id='username'
+							required
+							placeholder='Your Name'
+							className='
                                                                    bg-black
                                                                    p-3
                                                                    text-white
-                                                                   placeholder:text-brightRed'/>
+                                                                   placeholder:text-brightRed'
+						/>
 
-                                                                    <textarea 
-                                                                    id="comment"
-                                                                    className='bg-black p-2 
+						<textarea
+							id='comment'
+							className='bg-black p-2 
                                                                     text-white
-                                                                    placeholder:text-brightRed' 
-                                                                    required
-                                                                    name=""
-                                                                      placeholder='Write A comment'>
-                                                                  
-                                                                    </textarea>
+                                                                    placeholder:text-brightRed'
+							required
+							name=''
+							placeholder='Write A comment'
+						></textarea>
 
-                                                  
-                                                                    <div>
-                                                                      
-                                                                    <button className='p-3 font-bold  bg-black text-brightRed  
+						<div>
+							<button
+								className='p-3 font-bold  bg-black text-brightRed  
                                                                     hover:bg-veryLightBrown hover:text-black'
-                                                                      onClick={postComment}> 
-                                                                    Post Comment
-                                                                      </button>
-                                                                    </div>
-                                                        </form>
-                               </div>
+								onClick={postComment}
+							>
+								Post Comment
+							</button>
+						</div>
+					</form>
+				</div>
 
+				<div className='bg-stone-300 w-full p-5 md:w-4/6 '>
+					<h3 className='text-brightRed font-bold text-xl font-mono'>
+						Comments Sections
+					</h3>
 
-                               <div 
-                                className='bg-stone-300 w-full p-5 md:w-4/6 '>
-                                                <h3 
-                                                className='text-brightRed font-bold text-xl font-mono'>Comments Sections</h3>
+					<div className='mt-4 overflow-y-auto space-y-3 h-4/5'>
+						<div>
+							<p className='text-lg font-bold'> </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-                                                <div 
-                                                  className='mt-4 overflow-y-auto space-y-3 h-4/5'>
-
-                                                         <div>
-                                                         <p className='text-lg font-bold'> </p>
-
-                                                      
-                                                        
-
-                                                         </div>
-                                                </div>
-                               </div>
-
-              </div>
-            
-   </div>
-  )
-}
-
-export default SinglePost
+export default SinglePost;
