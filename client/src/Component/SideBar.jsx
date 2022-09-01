7a6768e8d68f0e2008cc771992d9031734704c17
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 15:40:36 +0100

    Update SideBar.jsx

--- client/src/Component/SideBar.jsx
+++ client/src/Component/SideBar.jsx
@@ -1,28 +1,102 @@
 import React from 'react';
-import {
-	ChatBubble,
-	Email,
-	Home,
-	Close,
-	Person,
-	SearchRounded,
-	LocalGroceryStore,
-	Settings,
-} from '@material-ui/icons';
+import { LocalCafeOutlined, Close } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import { useContext } from 'react';
 import { Context } from './context/Context';
 
 const SideBar = () => {
-	const { sideBar, setSidebar } = useContext(Context);
+	const { sideBar, setSideBar } = useContext(Context);
 	console.log(sideBar);
 	return (
 		<div
-			className={`relative z-50
-        ${sideBar ? 'block' : 'hidden'}  `}
+			className={`fixed md:hidden w-full bg-white h-screen z-50 transition-all duration-900
+        ${sideBar ? 'block right-0' : '-right-full'}  `}
 		>
-			<p> HEllo World </p>
+			{/**   Side Bar */}
+			<div className='flex flex-col p-5'>
+				<div
+					className=' w-full justify-between flex flex-row cursor-pointer'
+					onClick={() => setSideBar((prevsideBar) => !prevsideBar)}
+				>
+					<h2 className='text-5xl Phil font-bold md:text-center text-veryLightBrown'>
+						Cracked Ink
+					</h2>
+
+					<div>
+						<Close />
+					</div>
+				</div>
+
+				<ul className='w-full justify-between space-y-5 mt-5'>
+					<li className='w-full p-2 text-black'>
+						<a
+							href='/home'
+							className='font-serif cursor-pointer md:text-md
+                                        font-semibold
+                                        
+                                        hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							Home{' '}
+						</a>
+					</li>
+
+					<li className='w-full p-2 text-black'>
+						<a
+							href='/posts'
+							className='font-serif cursor-pointer md:text-md
+                                    font-semibold
+                                    hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							Write ups
+						</a>
+					</li>
+
+					<li className='w-full p-2 text-black'>
+						<a
+							href='/posts'
+							className='font-serif cursor-pointer md:text-md
+                                            font-semibold
+                                            hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							mail junks{' '}
+						</a>
+					</li>
+
+					<li className='w-full p-2 text-black'>
+						<a
+							href='/services'
+							className='font-serif cursor-pointer md:text-md
+                                                font-semibold
+                                                hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							Services{' '}
+						</a>
+					</li>
+
+					<li className='w-full p-2 text-black'>
+						<a
+							href='https://www.buymeacoffee.com/olatisun'
+							target='_blank'
+							rel='noreferrer'
+							className='font-serif cursor-pointer md:text-md
+                                          font-bold
+                                          text-brightRed'
+						>
+							{' '}
+							Buy ME Coffee <LocalCafeOutlined />{' '}
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
