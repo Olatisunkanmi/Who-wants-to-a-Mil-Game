2776411c1172d82a40c74709f060959b7c3e3d97
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 09:38:48 +0100

    Update AllPostsCard.jsx

--- client/src/Component/AllPostsCard.jsx
+++ client/src/Component/AllPostsCard.jsx
@@ -1,84 +1,71 @@
-import React from 'react'
-import '../App.css'
-import {Link, useLocation } from "react-router-dom"
-import {  useState } from "react";
-import { ArrowRightAlt, CommentOutlined, FavoriteBorderOutlined, Share } from "@material-ui/icons";
+import React from 'react';
+import '../App.css';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import {
+	ArrowRightAlt,
+	CommentOutlined,
+	FavoriteBorderOutlined,
+	Share,
+} from '@material-ui/icons';
 
-const AllPostsCard = ({post}) => {
-  const Path = useLocation()
- const  PostLikes = post.likes.length 
-  const [likes, setLikes] = useState(0);
-  const [isClicked, setIsClicked] = useState(false);
+const AllPostsCard = ({ post }) => {
+	const Path = useLocation();
+	const PostLikes = post.likes.length;
+	const [likes, setLikes] = useState(0);
+	const [isClicked, setIsClicked] = useState(false);
 
-  const handleClick = () => {
-    if (isClicked) {
-      setLikes(likes - 1);
-    } else {
-      setLikes(likes + 1);
-    }
-    setIsClicked(!isClicked);
-  };
+	const handleClick = () => {
+		if (isClicked) {
+			setLikes(likes - 1);
+		} else {
+			setLikes(likes + 1);
+		}
+		setIsClicked(!isClicked);
+	};
 
-  return (
-    <div className='w-full'>
-              <div className="flex flex-col md:flex-row justify-around text-center" >
+	return (
+		<div className='w-full'>
+			<div className='flex flex-col md:flex-row justify-around text-center'>
+				<div className='m-5 mb-2 '>
+					<div className='bg-white p-5  relative text-left'>
+						<h3 className='font-serif text-left Phil capitalize text-2xl tracking-wider font-semibold'>
+							<Link to={`/posts/${post._id}`}>{post.title}</Link>
+						</h3>
 
-                        <div 
-                          className="m-5 mb-2 ">
-                                    <div 
-                                      className="bg-white p-5  relative text-left">
-                                                        <h3
-                                                        className="font-serif text-left capitalize text-2xl tracking-wider font-semibold"> 
-                                                        <Link to={`/posts/${post._id}`}>
-                                                        {post.title} 
-                                                        </Link> 
-                                                        </h3>
+						<div className='mb-7 h-1 bg-zinc-700 border-none w-8'></div>
+						<p className='para'>{post.desc}</p>
 
-                                                        <div className="mb-7 h-1 bg-zinc-700 border-none w-8"></div>
-                                                      <p
-                                                        className="para">
-                                                        {post.desc}
-                                                      </p>
+						<div className='mt-5 Phil'>
+							<Link to={`/posts/${post._id}`}>
+								READ MORE
+								<ArrowRightAlt className='' />
+							</Link>
 
-                                                      <div  
-                                                      className="mt-5">
-                                                           
-                                                              <Link to={`/posts/${post._id}`}>
-                                                              READ MORE
-                                                            <ArrowRightAlt 
-                                                              className=""/>
-                                                            
-                                                            </Link>
+							<div className=' h-1 bg-zinc-700 border-none w-8'></div>
 
-                                                            <div className=" h-1 bg-zinc-700 border-none w-8"></div>
+							<div className='space-x-5 mt-5'>
+								<Share />
 
-                                                          <div  
-                                                            className="space-x-5 mt-5"> 
-                                                          <Share />
+								<button
+									className={`like-button ${isClicked && 'liked'}`}
+									onClick={handleClick}
+								>
+									<span className='likes-counter'>{`Like | ${likes}`}</span>
+								</button>
 
-                                                          <button className={ `like-button ${isClicked && 'liked'}` } onClick={ handleClick }>
-                                                          <span className="likes-counter">{ `Like | ${likes}` }</span>
-                                                        </button>
+								<FavoriteBorderOutlined className='cursor-pointer' />
+								<span>
+									{post.comments.length}
+									<CommentOutlined />{' '}
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-                                                          <FavoriteBorderOutlined  className='cursor-pointer' />
-                                                            <span> 
-                                                            {
-                                                              post.comments.length
-                                                            }
-                                                            
-                                                            <CommentOutlined />  </span>
-                                                          </div>
-
-                                                      </div>
-                                    </div>
-                        </div>
-
-
-                    
-
-              </div>
-    </div>
-  )
-}
-
-export default AllPostsCard
+export default AllPostsCard;
