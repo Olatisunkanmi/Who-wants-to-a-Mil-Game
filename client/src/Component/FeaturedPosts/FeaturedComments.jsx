a34cf70417e7f1c6e3413748fb5e5b6174aecadc
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 09:38:59 +0100

    Update FeaturedComments.jsx

--- client/src/Component/FeaturedPosts/FeaturedComments.jsx
+++ client/src/Component/FeaturedPosts/FeaturedComments.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
+import React from 'react';
 
+const FeaturedComments = ({ post }) => {
+	return (
+		<>
+			<p>HI </p>
+		</>
+	);
+};
 
-const FeaturedComments = ({post}) => {
-
-    
-  return (
-    <>
-                          <p>HI </p>
-                     
-    </>
-  )
-}
-
-export default FeaturedComments
+export default FeaturedComments;
