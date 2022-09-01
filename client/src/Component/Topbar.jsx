44e7acf674786255a41a8d281a26c93e4f262b18
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 09:04:22 +0100

    Update Topbar.jsx

--- client/src/Component/Topbar.jsx
+++ client/src/Component/Topbar.jsx
@@ -1,72 +1,92 @@
 import { LocalCafeOutlined } from '@material-ui/icons';
 
 const Topbar = () => {
-  return (
-
-        <div 
-                className=''>
-                        <div 
-                                className='
+	return (
+		<div className=''>
+			<div
+				className='
                                 lg:w-4/6
                                 m-auto 
                                 md:justify-around 
                                 flex flex-row 
                                 p-5 
                                 mt-7 border-4 border-y-black
-                                border-x-0'>
-                                <ul 
-                                        className='flex flex-row 
+                                border-x-0'
+			>
+				<ul
+					className='flex flex-row 
                                                         w-full
                                                         md:w-5/6 text-md  
-                                                        text-center justify-between  '>
-                                        <li  > 
-                                                 <a href='/home' className='font-serif cursor-pointer md:text-xl
+                                                        text-center justify-between  '
+				>
+					<li>
+						<a
+							href='/home'
+							className='font-serif cursor-pointer md:text-md
                                                 font-semibold
                                                 
-                                                hover:text-veryLightBrown' 
-                                                        rel="noreferrer" > Home </a>
-                                        </li>
+                                                hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							Home{' '}
+						</a>
+					</li>
 
-                                        <li> 
-                                                        <a href='/posts' className='font-serif cursor-pointer md:text-xl
+					<li>
+						<a
+							href='/posts'
+							className='font-serif cursor-pointer md:text-md
                                                 font-semibold
-                                                hover:text-veryLightBrown' 
-                                                        rel="noreferrer" > Write ups</a>
-                                        </li>
-                                        <li> 
-                                               
-                                                        <a href='/services' className='font-serif cursor-pointer md:text-xl
+                                                hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							Write ups
+						</a>
+					</li>
+					<li>
+						<a
+							href='/posts'
+							className='font-serif cursor-pointer md:text-md
                                                 font-semibold
-                                                hover:text-veryLightBrown' 
-                                                        rel="noreferrer" > Services </a>
-                                        </li>
-                                        <li> 
-                                             
-                                                        <a href='/'  className='font-serif cursor-pointer md:text-xl
+                                                hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							mail junks{' '}
+						</a>
+					</li>
+					<li>
+						<a
+							href='/services'
+							className='font-serif cursor-pointer md:text-md
                                                 font-semibold
-                                                hover:text-veryLightBrown' 
-                                                        rel="noreferrer"> About Me </a>
-                                        </li>
-                                      
-                                        <li >
-                                        <a
-                                        href='https://www.buymeacoffee.com/olatisun'
-                                        target='_blank'
-                                        rel="noreferrer"
-                                          className='font-serif cursor-pointer md:text-xl
+                                                hover:text-veryLightBrown'
+							rel='noreferrer'
+						>
+							{' '}
+							Services{' '}
+						</a>
+					</li>
+
+					<li>
+						<a
+							href='https://www.buymeacoffee.com/olatisun'
+							target='_blank'
+							rel='noreferrer'
+							className='font-serif cursor-pointer md:text-md
                                           font-bold
-                                          text-brightRed'> Buy ME Coffee <LocalCafeOutlined /> </a>
-                                        </li>
-                                       
-                                </ul>
-
-
-                             
-                        </div>
-        </div>
-        
-        
-  )
-}
+                                          text-brightRed'
+						>
+							{' '}
+							Buy ME Coffee <LocalCafeOutlined />{' '}
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
 export default Topbar;
