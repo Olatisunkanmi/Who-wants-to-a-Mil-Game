aed2248fbc03edcbae105c84467cdbce487825b7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-01 09:38:51 +0100

    Update Actions.js

--- client/src/Component/context/Actions.js
+++ client/src/Component/context/Actions.js
@@ -1,19 +1,17 @@
-export const LoginStart = (userCredentials ) => ({
-    type: "LOGIN_START"
-})
-
+export const LoginStart = (userCredentials) => ({
+	type: 'LOGIN_START',
+});
 
 // return user and login information
-export const LoginSuccess =    (user ) => ({
-    type: "LOGIN_SUCCESS",
-    payload: user,
-})
+export const LoginSuccess = (user) => ({
+	type: 'LOGIN_SUCCESS',
+	payload: user,
+});
 
+export const LoginFaluire = () => ({
+	type: 'LOGIN_FAILURE',
+});
 
-export const LoginFaluire = ( ) => ({
-    type: "LOGIN_FAILURE"
-})
-
-export const Logout = ( ) => ({
-    type: "LOGOUT"
-})
+export const Logout = () => ({
+	type: 'LOGOUT',
+});
