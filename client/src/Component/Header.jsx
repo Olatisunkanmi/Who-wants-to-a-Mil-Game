79a058b90b1c1f834ff138d01b990c0ec7b0e0bf
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-04 13:18:25 +0100

    Update Header.jsx

--- client/src/Component/Header.jsx
+++ client/src/Component/Header.jsx
@@ -1,11 +1,5 @@
-import { List, Menu } from '@material-ui/core';
 import '../App.css';
-import {
-	MenuBook,
-	MenuOutlined,
-	MenuSharp,
-	VerifiedUser,
-} from '@material-ui/icons';
+import { MenuOutlined } from '@material-ui/icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
@@ -15,12 +9,14 @@ const Header = () => {
 	const { sideBar, setSideBar } = useContext(Context);
 	return (
 		<div className='flex flex-row  '>
-			<div className=' w-11/12 md:w-full container max-w-6xl  px-5 p-3 mx-auto text-center '>
-				<Link to='/posts/' className='link'>
-					<h2 className='text-5xl font-bold md:text-center text-stone-800'>
-						Cracked Ink
-					</h2>
-				</Link>
+			<div className=' ]container max-w-6xl  px-5 p-3 mx-auto text-center '>
+				<a
+					href='./home'
+					className='text-5xl font-bold md:text-center text-stone-800'
+				>
+					Cracked Ink
+				</a>
+
 				<p className='italic text-sm md:text-xl font-bold mt-2'>
 					...Read, Write
 					<span className='text-veryLightBrown '>
