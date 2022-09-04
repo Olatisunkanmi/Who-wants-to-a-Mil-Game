34b8ee2c763a2add01f7ff4c3a5b389f866c6019
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-04 13:46:39 +0100

    Update Archives.jsx

--- client/src/Pages/Archives/Archives.jsx
+++ client/src/Pages/Archives/Archives.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Archives = () => {
 	return (
 		<div>
-			<div className=' lg:w-4/6 m-auto mt-20 space-y-3 md:flex md:justify-center md:text-center'>
+			<div className=' lg:w-4/6 m-auto mt-20 space-y-3 flex justify-center text-center'>
 				<div className='flex flex-col '>
 					<p className='tracking-widest text-3xl font-thin'>
 						Archives{' '}
@@ -12,11 +12,11 @@ const Archives = () => {
 					<p className='font-thin text-3xl  tracking-widest'> 2022</p>
 					<Link
 						to='/posts'
-						className=' text-center flex md:flex-row space-x-7 mt-10 font-thin Phil text-xl border-b-2 border-stone-600 p-2'
+						className=' text-center flex md:flex-row space-x-7 mt-10 font-thin Phil text-xl border-b-2 border-stone-600 p-3'
 					>
 						<p className=''>
-							{' '}
-							day <span> 199</span> :
+							<span className='hidden md:block'> day</span>{' '}
+							<span> 199</span> :
 						</p>
 						<p className=''>What to Do When You’re Feeling Drained</p>
 					</Link>
