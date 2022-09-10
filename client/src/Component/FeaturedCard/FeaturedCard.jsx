d9e4e62a0997bd0c50ebb468bd62486e0a51b00e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-10 22:27:45 +0100

    Update FeaturedCard.jsx

--- client/src/Component/FeaturedCard/FeaturedCard.jsx
+++ client/src/Component/FeaturedCard/FeaturedCard.jsx
@@ -1,41 +1,34 @@
-import { CardList  } from "../Index"
-import './FeaturedCard.css'
-import { useContext } from "react";
-import { Context } from "../context/Context";
-import { Link } from "react-router-dom";
-
+import { CardList } from '../Index';
+import './FeaturedCard.css';
+import { useContext } from 'react';
+import { Context } from '../context/Context';
+import { Link } from 'react-router-dom';
 
 const FeaturedCard = () => {
-  const { RandomPosts}  = useContext(Context)
+	const { mainPosts } = useContext(Context);
 
+	return (
+		<div className='justify-center mt-20 flex flex-col items-left max-w-5xl m-auto '>
+			<h3 className='Featured--Articles font-bold text-brightRed'>
+				{' '}
+				Featured Articles{' '}
+			</h3>
 
+			<div className='flex flex-col md:flex md:flex-row space-x-10'>
+				{mainPosts.map((cur) => (
+					<CardList key={cur._id} post={cur} />
+				))}
+			</div>
 
-  return (
+			<div>
+				<Link to='/posts'>
+					<button className='m-5 p-3 font-bold bg-black text-white hover:bg-veryLightBrown hover:text-black'>
+						View Older Posts
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-    <div className="justify-center mt-20 flex flex-col items-left max-w-5xl m-auto "> 
-             <h3 className="Featured--Articles font-bold text-brightRed"> Featured Articles </h3>
-    
-
-      <div className='flex flex-col md:flex md:flex-row space-x-10'>
-              {
-                RandomPosts.map ((cur) => (
-                <CardList key={cur._id} 
-                post= {cur} />   ))
-              
-              }
-      </div>
-         
-         
-          <div>
-          <Link to='/posts'>
-          <button className='m-5 p-3 font-bold bg-black text-white hover:bg-veryLightBrown hover:text-black'> 
-          View Older Posts
-          </button>
-          </Link>
-          </div>
-   </div>
-
-  )
-}
-
-export default FeaturedCard
+export default FeaturedCard;
