21018e3f7eca55c9bd62650a7c2da2b0029e1fac
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-10 22:27:47 +0100

    Update EachPostsPage.jsx

--- client/src/Component/EachPost/EachPostsPage.jsx
+++ client/src/Component/EachPost/EachPostsPage.jsx
@@ -3,13 +3,12 @@ import { useContext } from 'react';
 import { Context } from '../context/Context';
 
 const Posts = () => {
-	const { Allpost } = useContext(Context);
+	const { mainPosts } = useContext(Context);
 
 	return (
 		<div>
-			x
 			<div className='w-11/12 flex flex-col items-center lg:flex-row m-auto md:flex-wrap lg:w-4/6'>
-				{Allpost.map((cur) => (
+				{mainPosts.map((cur) => (
 					<AllPostsCard key={cur._id} post={cur} />
 				))}
 			</div>
