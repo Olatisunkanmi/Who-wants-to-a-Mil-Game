cea41b8c5feaa31335e083b48f69b386c6f6c843
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-10 22:27:42 +0100

    Update FirstCard.jsx

--- client/src/Component/FirstCard/FirstCard.jsx
+++ client/src/Component/FirstCard/FirstCard.jsx
@@ -1,59 +1,44 @@
-import {Link } from "react-router-dom";
-import {  CalendarTodayRounded} from  '@material-ui/icons'
+import { Link } from 'react-router-dom';
+import { CalendarTodayRounded } from '@material-ui/icons';
 import './FirstCard.css';
-import { useContext, useEffect, useState } from "react";
-import {Context } from '../context/Context'
-
-
+import { useContext, useEffect, useState } from 'react';
+import { Context } from '../context/Context';
 
 const FirstCard = ({}) => {
-const { newPost}  = useContext(Context)
+	const { mainPosts } = useContext(Context);
 
-            
+	const PF = 'https://cracked-ink-cv.herokuapp.com/images/';
+	// Send only last item to array to first card.
 
-      const PF =  "https://cracked-ink-cv.herokuapp.com/images/";
-      // Send only last item to array to first card.
+	// console.log(mainPosts);
+	return (
+		<div className='max-w-6xl px-5 mx-auto mt-32 text-center'>
+			<div className='text-xl font-bold text-center flex flex-col  items-center p-6 m-10 -mt-9 -mb-2'>
+				<img
+					src={PF + mainPosts.photo}
+					alt='image'
+					className='rounded'
+					width='850px'
+				/>
+			</div>
 
-        // console.log(newPost);
-  return (
-      
-    <div className='max-w-6xl px-5 mx-auto mt-32 text-center'>
+			<div className='flex flex-col items-center'>
+				<div className='flex items-center font-bold mb-10'>
+					<CalendarTodayRounded />
+					<span className=''>{`${new Date(
+						mainPosts.createdAt,
+					).toDateString()}.`}</span>
+				</div>
 
+				<span className='FirstCard--title'>{mainPosts.title}</span>
 
-        <div className='text-xl font-bold text-center flex flex-col  items-center p-6 m-10 -mt-9 -mb-2'>
-            <img src={PF + newPost.photo}
-            alt="image"  
-            className='rounded'
-            width='850px'/>
+				<span className='FirstCard--desc'>{mainPosts.desc}</span>
+				<button className='m-5 p-3 font-bold bg-black text-white hover:bg-veryLightBrown hover:text-black'>
+					<Link to={`/posts/${mainPosts._id}`}>Read More</Link>
+				</button>
+			</div>
+		</div>
+	);
+};
 
-      </div>
-
-
-            
-                    <div className="flex flex-col items-center">
-
-                                          <div className='flex items-center font-bold mb-10'>
-                                                <CalendarTodayRounded />
-                                                <span className="">{`${new Date(newPost.createdAt).toDateString()}.` }</span>
-
-                                          </div>
-
-
-                                    <span className="FirstCard--title">{newPost.title}</span>
-
-                                    <span className="FirstCard--desc">
-                                          {newPost.desc}
-                                    </span>
-                                    <button className='m-5 p-3 font-bold bg-black text-white hover:bg-veryLightBrown hover:text-black'> 
-                                                <Link to={`/posts/${newPost._id}`}>
-                                                Read More 
-                                                </Link>
-                                    </button>
-                    </div>
-
-    </div>
-
-  )
-}
-
-export default FirstCard
+export default FirstCard;
