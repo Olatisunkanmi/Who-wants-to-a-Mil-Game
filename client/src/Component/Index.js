e3bac1f37fc02c87fcf6b105789efed75d015366
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-11 20:10:18 +0100

    Update Index.js

--- client/src/Component/Index.js
+++ client/src/Component/Index.js
@@ -7,6 +7,7 @@ export { default as FirstCard } from './FirstCard/FirstCard';
 export { default as SinglePost } from './SinglePost';
 export { default as EachPosts } from './EachPost/EachPostsPage';
 export { default as AllPostsCard } from './AllPostsCard';
+export { default as AllPoemsCard } from './AllPoemsCard';
 export { default as SideBar } from './SideBar';
 export { default as ArchivesCard } from './ArchivesCard';
 export { default as SinglePoem } from './SinglePoem';
