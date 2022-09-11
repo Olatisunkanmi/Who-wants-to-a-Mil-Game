77bbe0730e3bd00630444ba7a427f357d12328e3
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-11 14:26:04 +0100

    Update Poems.jsx

--- client/src/Pages/Poems/Poems.jsx
+++ client/src/Pages/Poems/Poems.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { SinglePoem } from '../../Component/Index';
-
+import { SinglePoem, AllPostsCard } from '../../Component/Index';
+import { Context } from '../../Component/context/Context';
+import { useContext } from 'react';
 const Poems = () => {
+	const { poemPosts } = useContext(Context);
+	console.log(poemPosts);
+
 	return (
 		<div className='  lg:w-3/5 m-auto mt-10 space-y-8'>
-			<SinglePoem />
+			{poemPosts.map((cur) => (
+				<AllPostsCard key={cur._id} post={cur} />
+			))}
 		</div>
 	);
 };
