c44856e4bb45df9bef8ad9942f1e8e599d4bb18a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-13 13:43:25 +0100

    Update Write.jsx

--- client/src/Pages/Write/Write.jsx
+++ client/src/Pages/Write/Write.jsx
@@ -1,114 +1,110 @@
-import { useLocation , useNavigate } from "react-router-dom";
-import upload  from '../../Assests/upload.svg'
-import "./Write.css";
-import { useAuth } from "../../Hooks/useAuth";
-import {axios} from "axios";
-import { useState } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import upload from '../../Assests/upload.svg';
+import './Write.css';
+import { useAuth } from '../../Hooks/useAuth';
+import axios from 'axios';
+import { useState } from 'react';
 
-export default function Write( ) {
-  let navigate =  useNavigate()
-  const { user } = useAuth();
-  const [title, setTitle]  = useState(" ")
-  const [desc, setDesc]  = useState(" ")
-  const [file, setFile]  = useState(null)
+export default function Write() {
+	let navigate = useNavigate();
+	const { user } = useAuth();
+	const [title, setTitle] = useState(' ');
+	const [desc, setDesc] = useState(' ');
+	const [file, setFile] = useState(null);
 
-const HandleSubmit = async (e) => {
-  e.preventDefault();
+	const HandleSubmit = async (e) => {
+		e.preventDefault();
 
-  const newPost = {
-    username: user.username,
-    title: title,
-    desc: desc,
-  };
+		const newPost = {
+			title: title,
+			desc: desc,
+		};
 
+		if (file) {
+			const data = new FormData();
 
+			// prevent user from uploading diffrent images with same name
+			const filename = Date.now() + file.name;
+			console.log(filename);
+			data.append('name', filename);
+			data.append('file', file);
+			newPost.photo = filename;
 
-  if(file){
-    const data = new FormData();
+			console.log(typeof data);
+			console.log(data);
 
-    // prevent user from uploading diffrent images with same name 
-    const filename = Date.now()+ file.name;
-    data.append("name", filename);
-    data.append("file", file);
-    newPost.photo = filename;
+			try {
+				// upload Image
+				const res = await axios.post(
+					'http://localhost:3500/api/v2/upload',
+				);
+				console.log(res);
+			} catch (error) {
+				console.log(error);
+			}
+		} else {
+			alert('Insert Umage ');
+		}
 
-        try {
-              // upload Image
-              await axios.post("https://cracked-ink-cv.herokuapp.com/api/upload", data)
-              
-        } catch (error) {
-              console.log(error);
-        }
-  }
-        try {
-          const res = await axios.post("https://cracked-ink-cv.herokuapp.com/api/posts", newPost)
-        
-            navigate(`https://cracked-ink-cv.herokuapp.com/api/posts/${res.data._id}`)
-        } catch (error) {
-                console.log(error);
+		try {
+			const res = await axios.post(
+				'http://localhost:3500/api/v2/posts/',
+				newPost,
+			);
 
-        }
+			// navigate(
+			// 	`https://crackedinkv2.herokuapp.com/api/v2/posts/${res.data._id}`,
+			// );
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-}
-   
+	return (
+		<div className='write'>
+			{file ? (
+				<img
+					className='writeImg'
+					src={URL.createObjectURL(file)}
+					alt=''
+				/>
+			) : (
+				<img src={upload} className='writeImg' alt='' />
+			)}
 
-    //  const [newContent, SetNewContent] = useState(`${content}`)
-   const PublishPost =   () => {
-        // const res =  await axios.post(`/posts/`)
+			<form className='writeForm' onSubmit={HandleSubmit}>
+				<div className='writeFormGroup'>
+					<label htmlFor='fileInput'>
+						<i className='writeIcon fas fa-plus'></i>
+					</label>
 
-        console.log('object');
-   }
+					<input
+						id='fileInput'
+						type='file'
+						style={{ display: 'none' }}
+						onChange={(e) => setFile(e.target.files[0])}
+					/>
 
-  return (
+					<input
+						className='writeInput'
+						placeholder='Title'
+						type='text'
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</div>
+				<div className='writeFormGroup'>
+					<textarea
+						className='writeInput writeText'
+						placeholder='Tell your story...'
+						type='text'
+						onChange={(e) => setDesc(e.target.value)}
+					/>
+				</div>
 
-    <div className="write">
-
-     {
-      file 
-      ?  <img  className="writeImg"  src={URL.createObjectURL(file)}  alt=""   />
-
-      : <img src={upload} className="writeImg" alt=""/>
-     }
-
-
-            <form className="writeForm" onSubmit={HandleSubmit}>
-                <div className="writeFormGroup">
-                <label htmlFor="fileInput">
-                    <i className="writeIcon fas fa-plus"></i>
-                </label>
-                
-                
-                <input id="fileInput" type="file" style={{ display: "none" }}  onChange={(e) => setFile(e.target.files[0])}/>
-
-                <input
-                    className="writeInput"
-                    placeholder="Title"
-                    type="text"
-                    
-                    onChange= {e => setTitle(e.target.value)}
-                />
-                </div>
-                <div className="writeFormGroup">
-              
-
-              <textarea className="writeInput writeText" 
-              placeholder="Tell your story..." 
-              type="text" 
-  
-              onChange={e => setDesc(e.target.value)} />
-
-                
-
-
-                </div>
-
-                <button className="writeSubmit" type="submit"> Publish </button>
-            
-            
-            </form>
-
-
-
-    </div>
-  );
+				<button className='writeSubmit' type='submit'>
+					Publish
+				</button>
+			</form>
+		</div>
+	);
 }
