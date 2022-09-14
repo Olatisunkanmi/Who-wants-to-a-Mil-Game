3acbd71cefb5f02ed9f37273472b7fbf72486b8a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-14 15:06:33 +0100

    Update EachPoemPage.jsx

--- client/src/Pages/EachPoemPost.jsx/EachPoemPage.jsx
+++ client/src/Pages/EachPoemPost.jsx/EachPoemPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { AllPoemsCard } from '../../Component/Index';
-import { Context } from '../../Component/context/Context';
-import { useContext } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 const Poems = () => {
-	const { poemPosts } = useContext(Context);
+	const [poemPosts, setPoemPosts] = useState([]);
+
+	useEffect(() => {
+		const poemPosts = async () => {
+			const poemPosts = await axios.get(
+				'https://crackedinkv2.herokuapp.com/api/v2/posts/poems/',
+			);
+
+			setPoemPosts(poemPosts.data.data);
+		};
+		poemPosts();
+	});
+
 	return (
 		<div className='  lg:w-3/5 m-auto mt-10 space-y-8'>
 			{poemPosts.map((cur) => (
